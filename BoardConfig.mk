be4f8e84b0ed52690a959f45529a70ad1c60a19c
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-08-12 21:11:21 +0300

    cannon: Define TW_NO_SCREEN_BLANK
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,7 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
 
 # TWRP Configuration
+TW_NO_SCREEN_BLANK := true
 TW_THEME := portrait_hdpi
 TW_Y_OFFSET := 90
 TW_H_OFFSET := -70
